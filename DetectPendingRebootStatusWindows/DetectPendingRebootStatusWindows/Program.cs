142a99c654599e8ab296f7e29c8c04f177dd649c
Stefan Prugg <stefan.prugg@prugg.at>
2020-02-21 20:18:49 +0100

    initial commit

---
+++ DetectPendingRebootStatusWindows/DetectPendingRebootStatusWindows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WUApiLib;
+
+namespace DetectPendingRebootStatusWindows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISystemInformation systemInfo = new SystemInformation();
+
+            Console.WriteLine(systemInfo.RebootRequired.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
